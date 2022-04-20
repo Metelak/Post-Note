03e48a22836a63c99a74966dcdd38207ec1325f1
coyote <megan.metelak@gmail.com>
2022-04-20 18:38:43 -0500

    debugged and running on 3001

--- server.js
+++ server.js
@@ -1,67 +1,66 @@
 // connect to installed package paths
-const fs = require('fs');
-const path = require('path');
-const express = require('express');
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
 // connect notes to json file
-const { notes } = require('./db/db.json');
+const { notes } = require("./db/db.json");
 // Heroku running app through port
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
-function addNote(body, createdNotes) {
-    const note = body;
-    createdNotes.push(note);
-    fs.writeFileSync(
-        path.join(__dirname, './db/db.json'),
-        JSON.stringify({ notes: createdNotes }, null, 2)
-    );
-    return note;
+function createNewNote(body, notesArray) {
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, "./db/db.json"),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  );
+  return note;
 }
 
-function confirmNotes(notes) {
-    if (!notes.title || typeof notes.title !== 'string') {
-        return false;
-    }
-    if (!notes.text || typeof notes.text !== 'string') {
-        return false;
-    }
-    return true;
+function validateNotes(notes) {
+  if (!notes.title || typeof notes.title !== "string") {
+    return false;
+  }
+  if (!notes.text || typeof animal.species !== "string") {
+    return false;
+  }
+  return true;
 }
 
 // routing api/notes to read/return using json file
 app.post('/api/notes', (req, res) => {
-  // req.body.id = notes.length.toString();
-  console.log('new note received!');
-  const newNote = addNote(req.body, notes);
+  req.body.id = notes.length.toString();
+  console.log("new note received!");
+  const newNote = createNewNote(req.body, notes);
   res.json(newNote);
 });
 
 app.get('/api/notes', (req, res) => {
-    let result = notes;
-        res.json(result);
+  let results = notes;
+  res.json(results);
 });
 
 // read all notes from json file and remove note with given id property
 app.delete('/api/notes/:id', (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params;
 
-    const allNotes = notes.findIndex(p => p.id == id);
-    notes.splice(allNotes, 1);
-    res.send()
-})
-
+  const notesIndex = notes.findIndex((p) => p.id == id);
+  notes.splice(notesIndex, 1);
+  res.send();
+});
 
 // html routes
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
+  res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
 // app.get('/notes', (req, res) => {
@@ -69,7 +68,7 @@ app.get('/notes', (req, res) => {
 // });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
 // Default response for any other request (Not Found)
@@ -77,14 +76,12 @@ app.get('*', (req, res) => {
 //     res.status(404).end();
 // });
 
-
 module.exports = app;
 
 // the app is listening to the port variable defined
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+  console.log(`API server now on port ${PORT}!`);
 });
-
 
 // // Use fs for file management and path for directing files
 // const fs = require("fs");
